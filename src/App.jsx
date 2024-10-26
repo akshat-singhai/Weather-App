5b78e94eae4df880baca0d9a8c35b0fc91309dde
akshat-singhai <akshatsinghai2412@gmail.com>
2024-10-26 09:48:34 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather_app from './Component/Weather_app'
+const App = () => {
+  return (
+    <div className='App'>
+          <Weather_app/>
+    </div>
+  )
+}
+
+export default App
+
